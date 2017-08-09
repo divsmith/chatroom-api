a873cd6cd92b5e7d559e0ffefddd0d48c069be40
Parker Smith <parker@parkersmith.us>
2017-08-09 16:05:14 -0600

    Setup codeception

---
+++ tests/UserTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Test;
+
+
+class UserTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \Test\
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
