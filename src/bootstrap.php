ba22ffa37b8d343d6c7d80b77ec704c705c94ee1
Parker Smith <parker@parkersmith.us>
2017-08-16 14:53:57 -0600

    Work on routes

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -17,18 +17,18 @@ $container['userplugin'] = function($container) {
     return new \Storage\User\RedisUserPlugin();
 };
 
-$container['chatroomjack'] = function($container) {
-    $plugin = $this->get('chatroomplugin');
+$container['chatroomjack'] = function($container) use ($app) {
+    $plugin = $app->getContainer()->get('chatroomplugin');
     return new \Storage\Jacks\ChatRoomJack($plugin);
 };
 
-$container['messagejack'] = function($container) {
-    $plugin = $this->get('messageplugin');
+$container['messagejack'] = function($container) use ($app){
+    $plugin = $app->getContainer()->get('messageplugin');
     return new \Storage\Jacks\MessageJack($plugin);
 };
 
-$container['userjack'] = function($container) {
-    $plugin = $this->get('userplugin');
+$container['userjack'] = function($container) use ($app) {
+    $plugin = $app->getContainer()->get('userplugin');
     return new \Storage\Jacks\UserJack($plugin);
 };
 
--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,8 @@
 <?php
 // Routes
 
+use Domain\User;
+
 $app->get('/redis', function($request, $response, $args) {
     $client = new Predis\Client('tcp://172.18.0.2:6379');
     $client->set('foo', 'bar');
@@ -22,22 +24,102 @@ $app->get('/mysql', function($request, $response, $args) {
 
 $app->get('/chatroom', function($request, $response, $args) {
     // Return all chatrooms
+    $jack = $this->get('chatroomjack');
+
+    $rooms = $jack->getAll();
+
+    $json = [];
+
+    foreach ($rooms as $room)
+    {
+        $json[] = ['uuid' => $room->uuid(), 'name' => $room->name(), 'created' => $room->created(), 'updated' => $room->updated(), 'archived' => $room->archived()];
+    }
+
+    return $response->withJson($json);
 });
 
 $app->post('/chatroom', function($request, $response, $args) {
    // Create a chat room
+    $jack = $this->get('chatroomjack');
+
+    $name = $request->getParam('name');
+    $uuid = \Ramsey\Uuid\Uuid::uuid4();
+    $created = new DateTime('now');
+    $updated = new DateTime('now');
+
+    if ($jack->persist(new \Domain\ChatRoom($name, $created, $updated, $uuid)))
+    {
+        return $response->withJson(['chatroom' => '/chatroom/' . $uuid], 201);
+    }
+
+    return $response->withStatus(500);
+});
+
+$app->get('/chatroom/{uuid}', function($request, $response, $args) {
+    $jack = $this->get('chatroomjack');
+
+    $room = $jack->getByID($request->getAttribute('uuid'));
+
+    if ($room != null)
+    {
+        return $response->withJson(['uuid' => $room->uuid(), 'name' => $room->name(), 'created' => $room->created(), 'updated' => $room->updated(), 'archived' => $room->archived()]);
+    }
+
+    return $response->withStatus(404);
 });
 
 $app->post('/user', function($request, $response, $args) {
    // Create a user
+    $email = $request->getParam('email');
+    $alias = $request->getParam('alias');
+
+    $jack = $this->get('userjack');
+
+    try {
+        $jack->persist(new User($email, $alias));
+        return $response->withJson(['user' => '/user/' . $email], 201);
+    } catch(\Domain\Exceptions\InvalidEmailException $e) {
+        return $response->withJson(['error' => 'Invalid email address'], 406);
+    }
 });
 
-$app->put('/user', function($request, $response, $args) {
-   // Update user
+$app->patch('/user/{email}', function($request, $response, $args) {
+   // Update a user
 
-    // join chat room
+    $email = $request->getAttribute('email');
+    $alias = $request->getParsedBodyParam('alias');
+    $chatroomID = $request->getParsedBodyParam('chatroomID');
 
-    // leave chat room
+    $jack = $this->get('userjack');
+
+    $user = $jack->getByEmail($email);
+
+    if ($user == null)
+    {
+        return $response->withStatus(404);
+    }
+
+    $user->changeAlias($alias);
+    $user->changeChatRoomID($chatroomID);
+
+    $jack->persist($user);
+
+    return $response->withStatus(206);
+});
+
+$app->get('/user/{email}', function($request, $response, $args) {
+    $email = $request->getAttribute('email');
+
+    $jack = $this->get('userjack');
+
+    $user = $jack->getByEmail($email);
+
+    if ($user != null)
+    {
+        return $response->withJson(['email' => $user->email(), 'alias' => $user->alias(), 'chatroomID' => $user->chatRoomID()]);
+    }
+
+    return $response->withStatus(404);
 });
 
 $app->get('/user/{email}/chatroom/message', function() {
