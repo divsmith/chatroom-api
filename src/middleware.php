be0cac2681ff190d5ba6d48be8bb2b03324d8712
Parker Smith <parker@parkersmith.us>
2017-08-16 22:43:23 -0600

    Add client authorization middleware

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,14 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+$app->add(function($request, $response, $next) {
+    $headers = $request->getHeaders();
+
+    if ($headers['HTTP_TOKEN'][0] == "supersecrettoken")
+    {
+        return $next($request, $response);
+    }
+
+    //var_dump($headers);
+    return $response->withStatus(401);
+});
